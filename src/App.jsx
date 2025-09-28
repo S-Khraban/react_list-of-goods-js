818ad4308330a4808d0c4d54600fcd49e27bfd2c
S-Khraban <s.hraban@i.ua>
2025-09-28 19:04:31 +0300

    _create list of goods with diff sort buttons_

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import 'bulma/css/bulma.css';
+import React, { useMemo, useState } from 'react';
 import './App.scss';
 
-export const goodsFromServer = [
+const goodsFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -14,33 +14,87 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [sortBy, setSortBy] = useState(null);
+  const [isReversed, setIsReversed] = useState(false);
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  const displayed = useMemo(() => {
+    const result = [...goodsFromServer];
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+    if (sortBy === 'alpha') {
+      result.sort((a, b) => a.localeCompare(b));
+    } else if (sortBy === 'length') {
+      result.sort((a, b) => a.length - b.length || a.localeCompare(b));
+    }
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
-    </div>
+    if (isReversed) {
+      result.reverse();
+    }
 
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+    return result;
+  }, [sortBy, isReversed]);
+
+  const isInitialOrder = useMemo(() => {
+    return (
+      displayed.length === goodsFromServer.length &&
+      displayed.every((g, i) => g === goodsFromServer[i])
+    );
+  }, [displayed]);
+
+  return (
+    <main className="section container">
+      <h1 className="title">React list of goods</h1>
+
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${sortBy === 'alpha' ? '' : 'is-light'}`}
+          onClick={() => setSortBy('alpha')}
+          data-cy="SortByName"
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={`button is-success ${sortBy === 'length' ? '' : 'is-light'}`}
+          onClick={() => setSortBy('length')}
+          data-cy="SortByLength"
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={`button is-warning ${isReversed ? '' : 'is-light'}`}
+          onClick={() => setIsReversed(prev => !prev)}
+          data-cy="Reverse"
+        >
+          Reverse
+        </button>
+
+        {!isInitialOrder && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setSortBy(null);
+              setIsReversed(false);
+            }}
+            data-cy="Reset"
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul className="content" data-cy="GoodsList">
+        {displayed.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
